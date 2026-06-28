85557f1a68703cd858eb20f9fdf33e60f7e808ab
Prem Singh <premrathod0289@gmail.com>
2026-06-28 13:35:29 +0530

    update CORS configuration to allow multiple origins

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://repairmate-backend-1.onrender.com",
+    ],
     credentials: true,
   }),
 );
